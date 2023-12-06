5a8b1fb851983df71a2d6f0280fdbd9ab349c237
Vaishakh Anil <vaishakh.anil.360@gmail.com>
2023-12-06 12:39:38 -0500

    Contact Page - basic structure

--- script/script.js
+++ script/script.js
@@ -12,17 +12,26 @@ function hamburger() {
     menu = document.getElementById('hamburger');
     menuItems = document.getElementById('menu-items');
 
-    if(menu.classList.contains('close-button')) {
+    if (menu.classList.contains('close-button')) {
         menu.classList.remove('close-button');
-        menuItems.classList.add('desktop-tablet');
-        body.classList.remove('prevent-scroll');
-
         menuItems.classList.remove('mobile-menu-options');
+        body.classList.remove('prevent-scroll');
+        
+        menuItems.classList.add('desktop-tablet');
     } else {
         menu.classList.add('close-button');
         menuItems.classList.add('mobile-menu-options');
         body.classList.add('prevent-scroll');
-        
+
         menuItems.classList.remove('desktop-tablet');
     }
+}
+
+
+/**
+* Validate Form & 
+* Change Fryer based on the contact form Query
+*/
+function handleFormChange() {
+
 }
