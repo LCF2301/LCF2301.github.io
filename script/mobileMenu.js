c9a8d8603b5d42ea8fb002afa504d27998fef0bd
Vaishakh Anil <vaishakh.anil.360@gmail.com>
2023-12-06 03:29:20 -0500

    NavBar - Prevent scroll

--- script/mobileMenu.js
+++ script/mobileMenu.js
@@ -8,16 +8,20 @@
  * Hamburger Menu - Opens the navigation
  */
 function hamburger() {
+    body = document.querySelector('body');
     menu = document.getElementById('hamburger');
     menuItems = document.getElementById('menu-items');
 
     if(menu.classList.contains('close-button')) {
         menu.classList.remove('close-button');
-        menuItems.classList.remove('mobile-menu-options');
         menuItems.classList.add('desktop-tablet');
+        body.classList.remove('prevent-scroll');
+
+        menuItems.classList.remove('mobile-menu-options');
     } else {
         menu.classList.add('close-button');
-        menuItems.classList.add('mobile-menu-options')
+        menuItems.classList.add('mobile-menu-options');
+        body.classList.add('prevent-scroll');
         
         menuItems.classList.remove('desktop-tablet');
     }
